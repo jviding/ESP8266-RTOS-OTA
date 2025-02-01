8aedbbdeb808613d4dd255b103f5020298a047ff
jasu <jasu@jasubox.myguest.virtualbox.org>
2025-02-01 17:28:50 +0200

    use leds

--- main/main.cpp
+++ main/main.cpp
@@ -2,15 +2,27 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
+#include "leds.h"
 
-#define TAG "main"
+static const char* TAG = "Main";
 
-#define LED_1 GPIO_NUM_12
-#define LED_2 GPIO_NUM_14
+
+void setup() {
+  ESP_LOGI(TAG, "Started");
+  Leds::init();
+};
+
+void loop() {
+  ESP_LOGI(TAG, "Toggle Leds");
+  Leds::toggle_led_1();
+  Leds::toggle_led_2();
+  Leds::toggle_led_3();
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+};
 
 extern "C" void app_main() {
+  setup();
   while (true) {
-    ESP_LOGI("main", "Hello!");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    loop();
   }
 };
