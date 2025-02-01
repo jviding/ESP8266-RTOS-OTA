7397670dccceb9a0f6bc7d8c38bfb9b8e71d6d29
jasu <jasu@jasubox.myguest.virtualbox.org>
2025-02-02 01:20:53 +0200

    add print esp_err

--- components/esp_check/esp_check.h
+++ components/esp_check/esp_check.h
@@ -3,5 +3,6 @@
 #define ESP_RETURN_ON_ERROR(ret, tag, msg, ...) \
   if ((ret) != ESP_OK) { \
     ESP_LOGE((tag), (msg), ##__VA_ARGS__); \
+    ESP_LOGE((tag), "ERR: %s", esp_err_to_name(ret)); \
     return (ret); \
   }
