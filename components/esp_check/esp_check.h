c972534fa7a4774573890f8ba745e71832d6917c
jasu <jasu@jasubox.myguest.virtualbox.org>
2025-02-02 03:40:33 +0200

    goto cleanup macro

--- components/esp_check/esp_check.h
+++ components/esp_check/esp_check.h
@@ -6,3 +6,11 @@
     ESP_LOGE((tag), "ERR: %s", esp_err_to_name(ret)); \
     return (ret); \
   }
+
+#define ESP_CLEANUP_ON_ERROR(ret, tag, msg, ...) \
+  if ((ret) != ESP_OK) { \
+    esp_err = (ret); \
+    ESP_LOGE((tag), (msg), ##__VA_ARGS__); \
+    ESP_LOGE((tag), "ERR: %s", esp_err_to_name(ret)); \
+    goto cleanup; \
+  }
