1d2b66e2a97a0220ff4fdd259f6337a1563654a2
jasu <jasu@jasubox.myguest.virtualbox.org>
2025-03-30 04:28:57 +0300

    fix multiple calls to ret

--- components/esp_check/esp_check.h
+++ components/esp_check/esp_check.h
@@ -1,16 +1,22 @@
 #pragma once
 
 #define ESP_RETURN_ON_ERROR(ret, tag, msg, ...) \
-  if ((ret) != ESP_OK) { \
-    ESP_LOGE((tag), (msg), ##__VA_ARGS__); \
-    ESP_LOGE((tag), "ERR: %s", esp_err_to_name(ret)); \
-    return (ret); \
-  }
+  do { \
+    esp_err_t __err = (ret); \
+    if (__err != ESP_OK) { \
+      ESP_LOGE((tag), (msg), ##__VA_ARGS__); \
+      ESP_LOGE((tag), "ERR: %s", esp_err_to_name(__err)); \
+      return (__err); \
+    } \
+  } while (0);
 
-#define ESP_CLEANUP_ON_ERROR(ret, tag, msg, ...) \
-  if ((ret) != ESP_OK) { \
-    esp_err = (ret); \
-    ESP_LOGE((tag), (msg), ##__VA_ARGS__); \
-    ESP_LOGE((tag), "ERR: %s", esp_err_to_name(ret)); \
-    goto cleanup; \
-  }
+#define ESP_CLEANUP_ON_ERROR(ret, esp_err_var, tag, msg, ...) \
+  do { \
+    esp_err_t __err = (ret); \
+    if (__err != ESP_OK) { \
+        (esp_err_var) = (__err); \
+        ESP_LOGE((tag), (msg), ##__VA_ARGS__); \
+        ESP_LOGE((tag), "ERR: %s", esp_err_to_name(__err)); \
+        goto cleanup; \
+      } \
+  } while (0);
